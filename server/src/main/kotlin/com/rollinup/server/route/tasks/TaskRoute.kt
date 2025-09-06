2410f823d2d9738d6672febee579eb5cb52425cd
SetoAp69 <mocatastic77@gmail.com>
2025-09-06 19:32:35 +0700

    fix: fix bug on routing update: change suspendTransaction functions

--- server/src/main/kotlin/com/rollinup/server/route/tasks/TaskRoute.kt
+++ server/src/main/kotlin/com/rollinup/server/route/tasks/TaskRoute.kt
@@ -16,67 +16,67 @@ import org.koin.ktor.ext.inject
 fun Route.taskRoute() {
     val repository by inject<TaskRepository>()
 
+
     get {
-        get {
-            val searchQuery = call.request.queryParameters["search"]
-            val tasks = if (searchQuery.isNullOrBlank()) {
-                repository.getTask()
-            } else {
-                repository.search(searchQuery)
-            }
-            call.respond(tasks)
+        val searchQuery = call.request.queryParameters["search"]
+        val tasks = if (searchQuery.isNullOrBlank()) {
+            repository.getTask()
+        } else {
+            repository.search(searchQuery)
+        }
+        call.respond(tasks)
+    }
+
+    get("/byName/{taskName}") {
+        val name = call.parameters["taskName"]
+        val task = if (name.isNullOrBlank()) {
+            repository.getTask()
+        } else {
+            repository.getTaskByName(name)
         }
 
-        get("/byName/{taskName}") {
-            val name = call.parameters["taskName"]
-            val task = if (name.isNullOrBlank()) {
-                repository.getTask()
-            } else {
-                repository.getTaskByName(name)
-            }
+        call.respond(task)
+    }
+
+    get("/byPriority/{priority}") {
+        val sPriority = call.parameters["priority"]
+
+        if (sPriority.isNullOrBlank()) {
+            call.respond(HttpStatusCode.BadRequest)
+            return@get
+        }
+
+        try {
+            val priority = Priority.fromValue(sPriority)
+            val task = repository.getTaskByPriority(priority)
 
             call.respond(task)
-        }
-
-        get("/byPriority/{priority}") {
-            val sPriority = call.parameters["priority"]
-
-            if (sPriority.isNullOrBlank()) {
-                call.respond(HttpStatusCode.BadRequest)
-                return@get
-            }
-
-            try {
-                val priority = Priority.fromValue(sPriority)
-                val task = repository.getTaskByPriority(priority)
-
-                call.respond(task)
-            } catch (e: IllegalArgumentException) {
-                call.respond(HttpStatusCode.BadRequest)
-            }
-        }
-
-        post {
-            try {
-                val task = call.receive<Task>()
-                repository.addTask(task)
-                call.respond(HttpStatusCode.Created)
-
-            } catch (e: IllegalStateException) {
-                call.respond(HttpStatusCode.BadRequest)
-            }
-        }
-
-        delete("/{taskName}") {
-            val nameParams = call.parameters["taskName"]
-
-            if (nameParams.isNullOrBlank()) {
-                call.respond(HttpStatusCode.BadRequest)
-                return@delete
-            }
-
-            repository.removeTask(nameParams)
-            call.respond(HttpStatusCode.Accepted)
+        } catch (e: IllegalArgumentException) {
+            call.respond(HttpStatusCode.BadRequest)
         }
     }
+
+    post {
+        try {
+            val task = call.receive<Task>()
+            repository.addTask(task)
+            call.respond(HttpStatusCode.Created)
+
+        } catch (e: IllegalStateException) {
+            call.respond(HttpStatusCode.BadRequest)
+        }
+    }
+
+    delete("/{taskName}") {
+        val nameParams = call.parameters["taskName"]
+
+        if (nameParams.isNullOrBlank()) {
+            call.respond(HttpStatusCode.BadRequest)
+            return@delete
+        }
+
+        repository.removeTask(nameParams)
+        call.respond(HttpStatusCode.Accepted)
+    }
+
 }
