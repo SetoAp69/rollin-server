79fa7f217ec70164cd41fcde4dec16a6d514f006
SetoAp69 <mocatastic77@gmail.com>
2025-10-03 11:22:01 +0700

    chores : removes unused constant

--- server/src/main/kotlin/com/rollinup/server/Constant.kt
+++ server/src/main/kotlin/com/rollinup/server/Constant.kt
@@ -2,12 +2,10 @@ package com.rollinup.server
 
 object Constant {
 
-    const val EMPTY_TOKEN_ERROR = "Error Token was Empty"
-    const val RESPONSE_ERROR = "Error, failed to get response"
     const val DATABASE_DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSSSSS'Z'"
 
     
-    const val ACCESS_TOKEN_DURATION: Long = 600_000
+    const val ACCESS_TOKEN_DURATION: Long = 600_000_000
     const val REFRESH_TOKEN_DURATION: Long = 864_000_000
     const val OTP_DURATION: Long = 120_000
 }
